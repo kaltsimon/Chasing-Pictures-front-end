5881b318b6325f7c31a89bc2220d017c2bdae720
Simon <kaltsimon@gmail.com>
2015-05-30 19:06:26 +0200

    Basic (dummy) API connection for registration.

--- ChasingPictures/app/src/main/java/de/fu_berlin/cdv/chasingpictures/Register.java
+++ ChasingPictures/app/src/main/java/de/fu_berlin/cdv/chasingpictures/Register.java
@@ -1,6 +1,8 @@
 package de.fu_berlin.cdv.chasingpictures;
 
 import android.app.Activity;
+import android.content.res.Resources;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.Patterns;
@@ -9,7 +11,11 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
 import de.fu_berlin.cdv.chasingpictures.api.LoginRegistrationRequest;
+import de.fu_berlin.cdv.chasingpictures.api.LoginResult;
 
 
 public class Register extends Activity {
@@ -59,6 +65,56 @@ public class Register extends Activity {
         // TODO: salt & hash password?!
 
         LoginRegistrationRequest registrationRequest = new LoginRegistrationRequest(email, password);
-        // TODO: send request
+        RegistrationRequestTask requestTask = new RegistrationRequestTask();
+        requestTask.execute(registrationRequest);
+    }
+
+    private class RegistrationRequestTask extends AsyncTask<LoginRegistrationRequest, Void, LoginResult> {
+
+        @Override
+        protected LoginResult doInBackground(LoginRegistrationRequest... params) {
+            if (params.length != 0) {
+                try {
+                    Resources res = getResources();
+                    final String url = res.getString(R.string.api_main) + res.getString(R.string.api_register);
+                    RestTemplate restTemplate = buildJSONRestTemplate();
+                    return restTemplate.postForObject(url, params[0], LoginResult.class);
+                }
+                catch (Exception e) {
+                    Log.e(TAG, e.getMessage(), e);
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(LoginResult registrationResult) {
+            // DUMMY
+            registrationResult.setSuccessful(true);
+
+            Toast notification;
+
+            if (registrationResult.isSuccessful()) {
+                notification = Toast.makeText(getApplicationContext(), R.string.registration_success, Toast.LENGTH_SHORT);
+                notification.show();
+
+                // Return to previous view
+                setResult(RESULT_OK);
+                finish();
+            } else {
+                notification = Toast.makeText(getApplicationContext(), R.string.registration_fail, Toast.LENGTH_SHORT);
+                notification.show();
+            }
+        }
+    }
+
+    /**
+     * Builds a basic JSON rest template for sending requests.
+     * @return A RestTemplate with a {@see org.springframework.http.converter.json.Jackson2HttpMessageConverter} attached.
+     */
+    private static RestTemplate buildJSONRestTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+        return restTemplate;
     }
 }
