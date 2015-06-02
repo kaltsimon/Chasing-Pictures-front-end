642735fee7463c6d92032ba4fca2c2acafe61145
Simon <kaltsimon@gmail.com>
2015-06-03 00:14:33 +0200

    Added API URI retrieval function to ApiUtil

--- ChasingPictures/app/src/androidTest/java/de/fu_berlin/cdv/chasingpictures/api/ApiUtilTest.java
+++ ChasingPictures/app/src/androidTest/java/de/fu_berlin/cdv/chasingpictures/api/ApiUtilTest.java
@@ -3,10 +3,16 @@ package de.fu_berlin.cdv.chasingpictures.api;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
+import java.net.URI;
+
+import de.fu_berlin.cdv.chasingpictures.R;
+
 /**
  * @author Simon
  */
 public class ApiUtilTest extends ApplicationTestCase<Application> {
+
+    private ApiUtil apiUtil;
 
     public ApiUtilTest() {
         super(Application.class);
@@ -14,7 +20,7 @@ public class ApiUtilTest extends ApplicationTestCase<Application> {
 
     @Override
     protected void setUp() throws Exception {
-
+        apiUtil = new ApiUtil(getContext());
     }
 
     @Override
@@ -24,5 +30,15 @@ public class ApiUtilTest extends ApplicationTestCase<Application> {
 
     public void testCallSuccessful() throws Exception {
 
+    }
+
+    public void testGetURIforEndpoint() throws Exception {
+        String base_uri = getContext().getString(R.string.api_url);
+
+        URI expectedUriRegister = new URI(base_uri + getContext().getString(R.string.api_path_register));
+        assertEquals("Registration URIs differ", expectedUriRegister, apiUtil.getURIforEndpoint(R.string.api_path_register));
+
+        URI expectedUriLogin = new URI(base_uri + getContext().getString(R.string.api_path_login));
+        assertEquals("Login URIs differ", expectedUriLogin, apiUtil.getURIforEndpoint(R.string.api_path_login));
     }
 }
--- ChasingPictures/app/src/main/java/de/fu_berlin/cdv/chasingpictures/api/ApiUtil.java
+++ ChasingPictures/app/src/main/java/de/fu_berlin/cdv/chasingpictures/api/ApiUtil.java
@@ -5,6 +5,9 @@ import android.content.Context;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import de.fu_berlin.cdv.chasingpictures.R;
 
 /**
@@ -34,5 +37,16 @@ public class ApiUtil {
      */
     public boolean callSuccessful(ApiResult result) {
         return result.getStatus().equals(context.getString(R.string.api_status_success));
+    }
+
+    public URI getURIforEndpoint(int endpointId) {
+        try {
+            URI apiUrl = new URI(context.getString(R.string.api_url));
+            return apiUrl.resolve(context.getString(endpointId));
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
