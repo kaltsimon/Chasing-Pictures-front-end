67b334e8fdc83cde165dc07aadf8773d5658c853
Simon <kaltsimon@gmail.com>
2015-06-03 14:00:32 +0200

    Created Test for LoginPage

---
+++ ChasingPictures/app/src/androidTest/java/de/fu_berlin/cdv/chasingpictures/LoginPageTest.java
@@ -0,0 +1,23 @@
+package de.fu_berlin.cdv.chasingpictures;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * @author Simon
+ */
+public class LoginPageTest extends ApplicationTestCase<Application> {
+    public LoginPageTest() {
+        super(Application.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+
+    }
+}
