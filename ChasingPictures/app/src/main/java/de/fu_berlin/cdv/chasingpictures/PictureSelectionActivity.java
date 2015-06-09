4e8f3f40060bf12612550561e7464a1fac276b9b
Simon <kaltsimon@gmail.com>
2015-06-09 13:59:08 +0200

    Added basic GoogleAPI connection.

--- ChasingPictures/app/src/main/java/de/fu_berlin/cdv/chasingpictures/PictureSelectionActivity.java
+++ ChasingPictures/app/src/main/java/de/fu_berlin/cdv/chasingpictures/PictureSelectionActivity.java
@@ -1,14 +1,56 @@
 package de.fu_berlin.cdv.chasingpictures;
 
 import android.app.Activity;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import static com.google.android.gms.common.api.GoogleApiClient.*;
+import com.google.android.gms.location.LocationServices;
 
 
-public class PictureSelectionActivity extends Activity {
+public class PictureSelectionActivity extends Activity implements ConnectionCallbacks, OnConnectionFailedListener {
+    private static final String TAG = "PictureSelection";
+
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture_selection);
+        buildGoogleApiClient();
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+
+    /*
+    Google API callbacks
+     */
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        Log.d(TAG, mLastLocation.toString());
+        // TODO: Send request to backend
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG, "Connection to Google API services suspended.");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.e(TAG, "Connection to Google API services failed: " + connectionResult);
     }
 }
