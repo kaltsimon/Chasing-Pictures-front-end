64245db0c0ffe476f9c2848baf83d9aa188021d0
Simon <kaltsimon@gmail.com>
2015-06-09 13:26:55 +0200

    Added picture selection activity.

---
+++ ChasingPictures/app/src/main/java/de/fu_berlin/cdv/chasingpictures/PictureSelectionActivity.java
@@ -0,0 +1,14 @@
+package de.fu_berlin.cdv.chasingpictures;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class PictureSelectionActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picture_selection);
+    }
+}
