6df988e0921462c8545e1d21a10765bdf1a3969f
Simon <kaltsimon@gmail.com>
2015-06-09 15:32:01 +0200

    Added location API request.

---
+++ ChasingPictures/app/src/main/java/de/fu_berlin/cdv/chasingpictures/api/LocationRequest.java
@@ -0,0 +1,65 @@
+package de.fu_berlin.cdv.chasingpictures.api;
+
+import android.location.Location;
+
+/**
+ * @author Simon Kalt
+ */
+public class LocationRequest {
+    private double latitude;
+    private double longitude;
+
+    public LocationRequest() {}
+
+    public LocationRequest(Location location) {
+        setLatitude(location.getLatitude());
+        setLongitude(location.getLongitude());
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    @Override
+    public String toString() {
+        return "LocationRequest{" +
+                "latitude=" + latitude +
+                ", longitude=" + longitude +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        LocationRequest that = (LocationRequest) o;
+
+        if (Double.compare(that.latitude, latitude) != 0) return false;
+        return Double.compare(that.longitude, longitude) == 0;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        temp = Double.doubleToLongBits(latitude);
+        result = (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(longitude);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+}
