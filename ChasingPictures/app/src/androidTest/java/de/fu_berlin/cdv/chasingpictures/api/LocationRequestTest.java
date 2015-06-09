fd000a488c1e03a7f54fa414f656f1d85f34c5c9
Simon <kaltsimon@gmail.com>
2015-06-09 15:34:40 +0200

    Added test class for location requests.

---
+++ ChasingPictures/app/src/androidTest/java/de/fu_berlin/cdv/chasingpictures/api/LocationRequestTest.java
@@ -0,0 +1,26 @@
+package de.fu_berlin.cdv.chasingpictures.api;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Simon
+ */
+public class LocationRequestTest extends ApplicationTestCase<Application> {
+
+    public LocationRequestTest() {
+        super(Application.class);
+    }
+
+    @Override
+    public void setUp() throws Exception {
+
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+
+    }
+}
