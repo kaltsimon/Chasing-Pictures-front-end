71d3c72bdd2ff0ceef37bcbdab314ce79b2ae935
Simon <kaltsimon@gmail.com>
2015-06-10 12:17:09 +0200

    Extract location finding into separate abstract class.

---
+++ ChasingPictures/app/src/main/java/de/fu_berlin/cdv/chasingpictures/LocationHelper.java
@@ -0,0 +1,110 @@
+package de.fu_berlin.cdv.chasingpictures;
+
+import android.content.Context;
+import android.location.Location;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author Simon Kalt
+ */
+public abstract class LocationHelper implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+    private static final String TAG = "LocationHelper";
+    protected final Context mContext;
+    protected GoogleApiClient mGoogleApiClient;
+
+    private static final String MOCK_LOCATION_PROVIDER = "MockLocationProvider";
+    public LocationHelper(Context context) {
+        this.mContext = context;
+    }
+
+    public synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(mContext)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    public void connect() {
+        mGoogleApiClient.connect();
+    }
+
+    /**
+     * Call only after Call only after connecting!
+     * M
+     * @param mockLocation
+     */
+    public void useMockLocation(Location mockLocation) {
+        LocationServices.FusedLocationApi.setMockMode(mGoogleApiClient, true);
+        LocationServices.FusedLocationApi.setMockLocation(mGoogleApiClient, mockLocation);
+    }
+
+    /**
+     * Call only after connecting!
+     * @param latitude
+     * @param longitude
+     */
+    public void useMockLocation(double latitude, double longitude) {
+        Location mockLocation = new Location(MOCK_LOCATION_PROVIDER);
+        mockLocation.setLatitude(latitude);
+        mockLocation.setLongitude(longitude);
+        mockLocation.setAccuracy(100);
+        mockLocation.setTime(System.currentTimeMillis());
+
+        //region Complete Mock Location
+        Method locationJellyBeanFixMethod;
+        try {
+            locationJellyBeanFixMethod = Location.class.getMethod("makeComplete");
+            if (locationJellyBeanFixMethod != null) {
+                locationJellyBeanFixMethod.invoke(mockLocation);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //endregion
+
+        useMockLocation(mockLocation);
+    }
+
+    public GoogleApiClient getGoogleApiClient() {
+        return mGoogleApiClient;
+    }
+
+    public Location getLastLocation() {
+        return LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+    }
+
+    /**
+     *
+     * @return
+     */
+    public Location waitForLastLocation() {
+        Location lastLocation = getLastLocation();
+        while (lastLocation == null) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                Log.e(TAG, Log.getStackTraceString(e));
+            }
+            lastLocation = getLastLocation();
+        }
+
+        return lastLocation;
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG, "Connection to Google API services suspended.");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.e(TAG, "Connection to Google API services failed: " + connectionResult);
+    }
+}
--- ChasingPictures/app/src/main/java/de/fu_berlin/cdv/chasingpictures/PictureSelectionActivity.java
+++ ChasingPictures/app/src/main/java/de/fu_berlin/cdv/chasingpictures/PictureSelectionActivity.java
@@ -4,28 +4,19 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.location.Criteria;
+import android.content.Context;
 import android.location.Location;
 import android.location.LocationManager;
-import android.location.LocationProvider;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.ImageView;
-import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
 
 import org.springframework.http.ResponseEntity;
 
-import java.io.File;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
 import java.util.List;
 
 import de.fu_berlin.cdv.chasingpictures.PictureCard.OnFragmentInteractionListener;
@@ -33,19 +24,13 @@ import de.fu_berlin.cdv.chasingpictures.api.LocationRequest;
 import de.fu_berlin.cdv.chasingpictures.api.Place;
 import de.fu_berlin.cdv.chasingpictures.api.PlacesApiResult;
 
-import static com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
-import static com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 
-
-public class PictureSelectionActivity extends Activity implements ConnectionCallbacks, OnConnectionFailedListener, OnFragmentInteractionListener {
+public class PictureSelectionActivity extends Activity implements OnFragmentInteractionListener {
     private static final String TAG = "PictureSelection";
 
-    private GoogleApiClient mGoogleApiClient;
     private Location mLastLocation;
-    private LocationManager mLocationManager;
 
     //region MockLocation
-    private static final String MOCK_LOCATION_PROVIDER = "MockLocationProvider";
     private static final double MOCK_LOCATION_LAT = 52.517072;
     private static final double MOCK_LOCATION_LON = 13.388873;
     //endregion
@@ -55,29 +40,9 @@ public class PictureSelectionActivity extends Activity implements ConnectionCall
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture_selection);
 
-        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        setMockLocation(MOCK_LOCATION_LAT, MOCK_LOCATION_LON);
-
-        // This currently does not work!
-        // buildGoogleApiClient();
-
-        // Instead we manually get the mock location and send it.
-        Location lastKnownLocation = mLocationManager.getLastKnownLocation(MOCK_LOCATION_PROVIDER);
-        Log.d(TAG, "Last location: " + lastKnownLocation);
-
-        if (lastKnownLocation != null) {
-            new LocationTask().execute(lastKnownLocation);
-        } else {
-            Toast.makeText(this, "Could not read location...", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    protected synchronized void buildGoogleApiClient() {
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
+        LocationHelper locationHelper = new PictureViewLocationHelper(this);
+        locationHelper.buildGoogleApiClient();
+        locationHelper.connect();
     }
 
     @Override
@@ -116,77 +81,17 @@ public class PictureSelectionActivity extends Activity implements ConnectionCall
         }
     }
 
-    //region Google API callbacks
-    @Override
-    public void onConnected(Bundle connectionHint) {
-        Log.d(TAG, "Connected to Google API services.");
-
-        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-
-        new LocationTask().execute(mLastLocation);
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        Log.d(TAG, "Connection to Google API services suspended.");
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-        Log.e(TAG, "Connection to Google API services failed: " + connectionResult);
-    }
-    //endregion
-
-    //region MockLocation
-    public void setMockLocation(double latitude, double longitude) {
-        if (mLocationManager.getProvider(MOCK_LOCATION_PROVIDER) != null) {
-            mLocationManager.removeTestProvider(MOCK_LOCATION_PROVIDER);
+    private class PictureViewLocationHelper extends LocationHelper {
+        public PictureViewLocationHelper(Context context) {
+            super(context);
         }
 
-        mLocationManager.addTestProvider(
-                MOCK_LOCATION_PROVIDER,
-                false, // requires network
-                false, // requires satellite
-                false, // requires cell
-                false, // has monetary cost
-                false, // supports altitude
-                false, // supports speed
-                false, // supports bearing
-                Criteria.POWER_LOW,
-                Criteria.ACCURACY_FINE
-        );
-
-        Location mockLocation = new Location(MOCK_LOCATION_PROVIDER);
-        mockLocation.setLatitude(latitude);
-        mockLocation.setLongitude(longitude);
-        mockLocation.setAccuracy(100);
-        mockLocation.setTime(System.currentTimeMillis());
-
-        //region Complete Mock Location
-        Method locationJellyBeanFixMethod;
-        try {
-            locationJellyBeanFixMethod = Location.class.getMethod("makeComplete");
-            if (locationJellyBeanFixMethod != null) {
-                locationJellyBeanFixMethod.invoke(mockLocation);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+        @Override
+        public void onConnected(Bundle connectionHint) {
+            Log.d(TAG, "Connected to Google API services.");
+            useMockLocation(MOCK_LOCATION_LAT, MOCK_LOCATION_LON);
+            mLastLocation = waitForLastLocation();
+            new LocationTask().execute(mLastLocation);
         }
-        //endregion
-
-        mLocationManager.setTestProviderEnabled(MOCK_LOCATION_PROVIDER, true);
-
-        mLocationManager.setTestProviderStatus(
-                MOCK_LOCATION_PROVIDER,
-                LocationProvider.AVAILABLE,
-                null,
-                System.currentTimeMillis()
-        );
-
-        mLocationManager.setTestProviderLocation(
-                MOCK_LOCATION_PROVIDER,
-                mockLocation
-        );
     }
-    //endregion
 }
