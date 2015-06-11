0d0766d2c7eb7979110b8151b2920044e4b5c19b
Simon <kaltsimon@gmail.com>
2015-06-11 14:42:23 +0200

    Moved login/registration tests.

--- ChasingPictures/app/src/androidTest/java/de/fu_berlin/cdv/chasingpictures/api/LoginRequestTest.java
+++ ChasingPictures/app/src/androidTest/java/de/fu_berlin/cdv/chasingpictures/api/LoginRequestTest.java
@@ -5,10 +5,12 @@ import android.content.Context;
 import android.test.ApplicationTestCase;
 import android.util.Base64;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.security.SecureRandom;
+import java.util.List;
 
 import de.fu_berlin.cdv.chasingpictures.security.Access;
 
@@ -28,6 +30,47 @@ public class LoginRequestTest extends ApplicationTestCase<Application> {
         ResponseEntity<RegistrationApiResult> responseEntity;
         responseEntity = registerUser(context, NAME, getUniqueEmail(), PASSWORD);
         checkAccess(context, responseEntity);
+    }
+
+    public void testResponseHeaders() throws Exception {
+        LoginRequest<RegistrationApiResult> request;
+        request = LoginRequest.makeRegistrationRequest(
+                getContext(), NAME, getUniqueEmail(), PASSWORD);
+        ResponseEntity<RegistrationApiResult> response = request.send();
+
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
+        HttpHeaders headers = response.getHeaders();
+        assertNotNull(headers);
+
+        // Check that all the access headers are there
+        for (Access.Headers header : Access.Headers.values()) {
+            List<String> values = headers.get(header.field);
+            assertNotNull(values);
+            assertFalse("Header field " + header.field + " is missing", values.isEmpty());
+            String value = values.get(0);
+            assertNotNull("Header value for " + header.field + " is null", value);
+            assertFalse("Header value for " + header.field + " is empty", value.isEmpty());
+        }
+
+        // We can assume this exists now
+        Long expiry = Long.valueOf(headers.get(Access.Headers.EXPIRY.field).get(0));
+        assertTrue("Access-Token is expired", System.currentTimeMillis() / 1000 < expiry);
+    }
+
+    public void testBlankEmailError() throws Exception {
+        // Perform request
+        RegistrationApiResult apiResult = registerUser(getContext(), NAME, "", PASSWORD).getBody();
+
+        assertEquals("API return status is wrong.", "error", apiResult.getStatus());
+        assertEquals("Email error message is wrong.", "can't be blank", apiResult.getErrors().getErrorMessages().get("email").get(0));
+    }
+
+    public void testBlankPasswordError() throws Exception {
+        // Perform request
+        RegistrationApiResult apiResult = registerUser(getContext(), NAME, getUniqueEmail(), "").getBody();
+
+        assertEquals("API return status is wrong.", "error", apiResult.getStatus());
+        assertEquals("Password error message is wrong.", "can't be blank", apiResult.getErrors().getErrorMessages().get("password").get(0));
     }
 
     public void testMakeLoginRequest() throws Exception {
