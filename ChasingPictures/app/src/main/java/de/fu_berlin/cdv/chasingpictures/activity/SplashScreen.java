f2581253ffd90b12a887bbbef3aa04da3b7883de
Simon <kaltsimon@gmail.com>
2015-06-12 15:26:19 +0200

    Added splash screen activity.

---
+++ ChasingPictures/app/src/main/java/de/fu_berlin/cdv/chasingpictures/activity/SplashScreen.java
@@ -0,0 +1,17 @@
+package de.fu_berlin.cdv.chasingpictures.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import de.fu_berlin.cdv.chasingpictures.R;
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+
+
+}
