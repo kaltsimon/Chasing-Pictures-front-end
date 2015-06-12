06630328a5d02b2325f448d6abb82e53bd964af3
Simon <kaltsimon@gmail.com>
2015-06-12 15:48:08 +0200

    Start splash screen at start of app and start main activity after a short delay.

--- ChasingPictures/app/src/main/java/de/fu_berlin/cdv/chasingpictures/activity/SplashScreen.java
+++ ChasingPictures/app/src/main/java/de/fu_berlin/cdv/chasingpictures/activity/SplashScreen.java
@@ -1,11 +1,16 @@
 package de.fu_berlin.cdv.chasingpictures.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
+import de.fu_berlin.cdv.chasingpictures.MainActivity;
 import de.fu_berlin.cdv.chasingpictures.R;
 
 public class SplashScreen extends Activity {
+
+    private static final long SPLASH_DELAY_MILLIS = 300;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +18,19 @@ public class SplashScreen extends Activity {
         setContentView(R.layout.activity_splash_screen);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
 
+        // Start the main activity after a short delay.
+        new Handler().postDelayed(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    }
+                },
+                SPLASH_DELAY_MILLIS
+        );
+    }
 }
