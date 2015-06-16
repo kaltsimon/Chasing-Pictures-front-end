df207a00bfd3ad3f20261fc0c55e5e505f66258d
Simon <kaltsimon@gmail.com>
2015-06-16 18:38:39 +0200

    Automatically check if a user is logged in on resume of main activity. If not -> go to login page.

--- ChasingPictures/app/src/main/java/de/fu_berlin/cdv/chasingpictures/MainActivity.java
+++ ChasingPictures/app/src/main/java/de/fu_berlin/cdv/chasingpictures/MainActivity.java
@@ -2,21 +2,53 @@ package de.fu_berlin.cdv.chasingpictures;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import de.fu_berlin.cdv.chasingpictures.security.Access;
 
 
 public class MainActivity extends Activity {
     private static final String TAG = "MainActivity";
+    private boolean triedLogin = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.intro);
-        mediaPlayer.start(); // no need to call prepare(); create() does that for you
+//        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.intro);
+//        mediaPlayer.start(); // no need to call prepare(); create() does that for you
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Check if we are logged in, but only once
+        if (!triedLogin && !Access.hasAccess(this)) {
+            triedLogin = true;
+            Intent intent = new Intent(this, LoginPage.class);
+            startActivityForResult(intent, LoginPage.LOGIN);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == LoginPage.LOGIN
+                && resultCode == RESULT_OK
+                && Access.hasAccess(this)) {
+            Toast.makeText(this, R.string.login_success, Toast.LENGTH_SHORT).show();
+            findViewById(R.id.show_login_page_button).setEnabled(false);
+        }
+    }
+
+    public void logOut(View view) {
+        Access.revokeAccess(this);
+        if (!Access.hasAccess(this)) {
+            Toast.makeText(this, getString(R.string.logout_success), Toast.LENGTH_SHORT).show();
+            findViewById(R.id.show_login_page_button).setEnabled(true);
+        }
     }
 
     public void showLoginPage(View view) {
