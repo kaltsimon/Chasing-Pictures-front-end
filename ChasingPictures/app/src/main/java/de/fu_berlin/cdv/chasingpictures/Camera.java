09dde5ab8fd5327c6c2d4f131df3a8942753bf8d
miriwie <miri.wieseke@web.de>
2015-06-17 10:40:16 +0200

    catch case of user stopping taking picture

--- ChasingPictures/app/src/main/java/de/fu_berlin/cdv/chasingpictures/Camera.java
+++ ChasingPictures/app/src/main/java/de/fu_berlin/cdv/chasingpictures/Camera.java
@@ -7,6 +7,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 
 public class Camera extends Activity {
@@ -24,17 +25,32 @@ public class Camera extends Activity {
         iv = (ImageView) findViewById(R.id.imageView);
 
         Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(intent, 0);
+        startActivityForResult(intent, 100);
 
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         // TODO Auto-generated method stub
-        super.onActivityResult(requestCode, resultCode, data);
+        //super.onActivityResult(requestCode, resultCode, data);
 
-        Bitmap bp = (Bitmap) data.getExtras().get("data");
-        iv.setImageBitmap(bp);
+        Intent intent = new Intent(this, MainActivity.class);
+
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // Image captured and saved to fileUri specified in the Intent
+                Toast.makeText(this, "Image saved to:\n" + data.getData(), Toast.LENGTH_LONG).show();
+                Bitmap bp = (Bitmap) data.getExtras().get("data");
+                iv.setImageBitmap(bp);
+            } else if (resultCode == RESULT_CANCELED) {
+                // User cancelled the image capture
+
+                startActivity(intent);
+            } else {
+                // Image capture failed, advise user
+                startActivity(intent);
+            }
+        }
 
     }
 
