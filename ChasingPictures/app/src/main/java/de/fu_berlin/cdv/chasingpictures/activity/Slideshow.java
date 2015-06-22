6844750014dc4649b4b2872f10095e68f6046036
Simon <kaltsimon@gmail.com>
2015-06-22 15:03:11 +0200

    Create Slideshow intent with list of pictures

--- ChasingPictures/app/src/main/java/de/fu_berlin/cdv/chasingpictures/activity/Slideshow.java
+++ ChasingPictures/app/src/main/java/de/fu_berlin/cdv/chasingpictures/activity/Slideshow.java
@@ -1,11 +1,24 @@
 package de.fu_berlin.cdv.chasingpictures.activity;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
+import java.io.Serializable;
+import java.util.List;
 import de.fu_berlin.cdv.chasingpictures.R;
+import de.fu_berlin.cdv.chasingpictures.api.Picture;
 
 public class Slideshow extends Activity {
+
+    private static final String PICTURES_EXTRA = "de.fu_berlin.cdv.chasingpictures.EXTRA_PICTURES";
+
+    public static Intent createIntent(Context context, List<Picture> pictures) {
+        Intent intent = new Intent(context, Slideshow.class);
+        intent.putExtra(PICTURES_EXTRA, (Serializable) pictures);
+        return intent;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
