8756b402b6e6eb39ad6ee4d087a6bba1e5425f00
Simon <kaltsimon@gmail.com>
2015-06-22 13:49:51 +0200

    Add picture request

---
+++ ChasingPictures/app/src/main/java/de/fu_berlin/cdv/chasingpictures/api/PictureRequest.java
@@ -0,0 +1,38 @@
+package de.fu_berlin.cdv.chasingpictures.api;
+
+import android.content.Context;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import de.fu_berlin.cdv.chasingpictures.R;
+
+/**
+ * @author Simon Kalt
+ */
+public class PictureRequest extends ApiRequest<PictureApiResult> {
+    private final Place place;
+
+    protected PictureRequest(Context context, Place place) {
+        super(context, R.string.api_path_picture);
+        this.place = place;
+    }
+
+    @Override
+    protected ResponseEntity<PictureApiResult> send() {
+        Map<String, Integer> queryParameters = new HashMap<>(2);
+        queryParameters.put("placeId", place.getId());
+
+        return restTemplate.exchange(
+                apiUri,
+                HttpMethod.GET,
+                new HttpEntity<>(headers),
+                PictureApiResult.class,
+                queryParameters
+        );
+    }
+}
