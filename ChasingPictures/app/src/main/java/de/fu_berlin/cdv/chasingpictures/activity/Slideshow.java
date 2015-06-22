fb0deaf7ec231e046c94cbcc1ccfddd43e09be63
Simon <kaltsimon@gmail.com>
2015-06-22 14:16:08 +0200

    Add slideshow activity

---
+++ ChasingPictures/app/src/main/java/de/fu_berlin/cdv/chasingpictures/activity/Slideshow.java
@@ -0,0 +1,15 @@
+package de.fu_berlin.cdv.chasingpictures.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import de.fu_berlin.cdv.chasingpictures.R;
+
+public class Slideshow extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slideshow);
+    }
+}
