08c797b017b64cb74e5534d34e25e75bf0bdfd66
Simon <kaltsimon@gmail.com>
2015-06-24 01:50:52 +0200

    Optimize imports

--- ChasingPictures/app/src/main/java/de/fu_berlin/cdv/chasingpictures/activity/Slideshow.java
+++ ChasingPictures/app/src/main/java/de/fu_berlin/cdv/chasingpictures/activity/Slideshow.java
@@ -17,7 +17,6 @@ import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import java.io.File;
-import java.io.Serializable;
 import java.util.List;
 
 import de.fu_berlin.cdv.chasingpictures.Maps;
