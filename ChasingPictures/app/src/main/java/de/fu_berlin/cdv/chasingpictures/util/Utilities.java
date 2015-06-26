2bd402e8504963026704e9ddbfd8fd038273ca53
Simon <kaltsimon@gmail.com>
2015-06-26 17:56:37 +0200

    Allow Utilities toasts to be shown on a specified handler

--- ChasingPictures/app/src/main/java/de/fu_berlin/cdv/chasingpictures/util/Utilities.java
+++ ChasingPictures/app/src/main/java/de/fu_berlin/cdv/chasingpictures/util/Utilities.java
@@ -3,6 +3,7 @@ package de.fu_berlin.cdv.chasingpictures.util;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Handler;
+import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
 import android.widget.Toast;
 
@@ -21,6 +22,18 @@ public class Utilities {
      */
     @Deprecated
     public static void showError(Context context, String formatString, Object... args) {
+        showError(context, formatString, null, args);
+    }
+
+    /**
+     *
+     * @param context The current context
+     * @param formatString The message to be displayed
+     * @param handler The handler to run Toast.show on
+     *@param args Arguments to be formatted into the string  @deprecated Use {@link #showError(Context, int, Object...)} if possible.
+     */
+    @Deprecated
+    public static void showError(Context context, String formatString, @Nullable Handler handler, Object... args) {
         @SuppressLint("ShowToast")
         final Toast toast = Toast.makeText(
                 context,
@@ -28,14 +41,16 @@ public class Utilities {
                 Toast.LENGTH_SHORT
         );
 
-        // Display on main looper.
-        Handler handler = new Handler(context.getMainLooper());
-        handler.post(new Runnable() {
-            @Override
-            public void run() {
-                toast.show();
-            }
-        });
+        if (handler != null) {
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    toast.show();
+                }
+            });
+        } else {
+            toast.show();
+        }
     }
 
     /**
@@ -47,7 +62,20 @@ public class Utilities {
      * @param args Arguments to be formatted into the string
      */
     public static void showError(Context context, @StringRes int resID, Object... args) {
-        showToast(context, resID, args);
+        showError(context, resID, null, args);
+    }
+
+    /**
+     * Displays an error to the user.
+     * Currently does the same thing as {@link #showToast(Context, int, Object...)} but
+     * could later be changed to look differently.
+     * @param context The current context
+     * @param resID A string resource to be displayed
+     * @param handler
+     * @param args Arguments to be formatted into the string
+     */
+    public static void showError(Context context, @StringRes int resID, @Nullable Handler handler, Object... args) {
+        showToast(context, resID, handler, args);
     }
 
     /**
@@ -57,6 +85,17 @@ public class Utilities {
      * @param args Arguments to be formatted into the string
      */
     public static void showToast(Context context, @StringRes final int resID, final Object... args) {
+        showToast(context, resID, null, args);
+    }
+
+    /**
+     * Displays a message to the user.
+     * @param context The current context
+     * @param resID A string resource to be displayed
+     * @param handler
+     * @param args Arguments to be formatted into the string
+     */
+    public static void showToast(Context context, @StringRes final int resID, @Nullable Handler handler, final Object... args) {
         @SuppressLint("ShowToast")
         final Toast toast = Toast.makeText(
                 context,
@@ -65,12 +104,15 @@ public class Utilities {
         );
 
         // Display on main looper.
-        Handler handler = new Handler(context.getMainLooper());
-        handler.post(new Runnable() {
-            @Override
-            public void run() {
-                toast.show();
-            }
-        });
+        if (handler != null) {
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    toast.show();
+                }
+            });
+        } else {
+            toast.show();
+        }
     }
 }
