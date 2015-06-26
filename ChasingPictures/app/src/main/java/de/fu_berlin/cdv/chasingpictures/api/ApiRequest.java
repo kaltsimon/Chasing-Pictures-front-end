ab914cb11aff8a6efde3f8af122dcddf2431ae26
Simon <kaltsimon@gmail.com>
2015-06-26 18:08:22 +0200

    Remove showError call from ApiRequest exception handling since it can only be done from the UI thread

--- ChasingPictures/app/src/main/java/de/fu_berlin/cdv/chasingpictures/api/ApiRequest.java
+++ ChasingPictures/app/src/main/java/de/fu_berlin/cdv/chasingpictures/api/ApiRequest.java
@@ -91,7 +91,6 @@ public abstract class ApiRequest<ResponseType> {
      */
     protected void handleException(Exception ex) {
         Log.e(TAG, "An exception occurred while sending the request.", ex);
-        Utilities.showError(context, R.string.api_error_server_unreachable);
     }
 
     /**
