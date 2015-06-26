95ff8a22109d1ab71b4173997c270d866111557e
Simon <kaltsimon@gmail.com>
2015-06-26 18:26:22 +0200

    Use xdroid Toaster instead of wrangling around with Handlers

--- ChasingPictures/app/src/main/java/de/fu_berlin/cdv/chasingpictures/util/Utilities.java
+++ ChasingPictures/app/src/main/java/de/fu_berlin/cdv/chasingpictures/util/Utilities.java
@@ -7,6 +7,8 @@ import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
 import android.widget.Toast;
 
+import xdroid.toaster.Toaster;
+
 /**
  * Class that provides utility methods.
  *
@@ -21,34 +23,8 @@ public class Utilities {
      */
     @Deprecated
     public static void showError(Context context, String formatString, Object... args) {
-        showError(context, formatString, null, args);
-    }
-
-    /**
-     * @param context      The current context
-     * @param formatString The message to be displayed
-     * @param handler      The handler to post {@link Toast#show()} to
-     * @param args         Arguments to be formatted into the string  @deprecated Use {@link #showError(Context, int, Object...)} if possible.
-     */
-    @Deprecated
-    public static void showError(Context context, String formatString, @Nullable Handler handler, Object... args) {
-        @SuppressLint("ShowToast")
-        final Toast toast = Toast.makeText(
-                context,
-                String.format(formatString, args),
-                Toast.LENGTH_SHORT
-        );
-
-        if (handler != null) {
-            handler.post(new Runnable() {
-                @Override
-                public void run() {
-                    toast.show();
-                }
-            });
-        } else {
-            toast.show();
-        }
+        String message = args.length == 0 ? formatString : String.format(formatString, args);
+        Toaster.toast(message);
     }
 
     /**
@@ -61,21 +37,7 @@ public class Utilities {
      * @param args    Arguments to be formatted into the string
      */
     public static void showError(Context context, @StringRes int resID, Object... args) {
-        showError(context, resID, null, args);
-    }
-
-    /**
-     * Displays an error to the user.
-     * Currently does the same thing as {@link #showToast(Context, int, Object...)} but
-     * could later be changed to look differently.
-     *
-     * @param context The current context
-     * @param resID   A string resource to be displayed
-     * @param handler The handler to post {@link Toast#show()} to
-     * @param args    Arguments to be formatted into the string
-     */
-    public static void showError(Context context, @StringRes int resID, @Nullable Handler handler, Object... args) {
-        showToast(context, resID, handler, args);
+        showToast(context, resID, args);
     }
 
     /**
@@ -86,7 +48,9 @@ public class Utilities {
      * @param args    Arguments to be formatted into the string
      */
     public static void showToast(Context context, @StringRes final int resID, final Object... args) {
-        showToast(context, resID, null, args);
+        String message = context.getString(resID);
+        message = args.length == 0 ? message : String.format(message, args);
+        Toaster.toast(message);
     }
 
     /**
