4e81f4af04589d9eb8d80b760c49909af4409e3b
Simon <kaltsimon@gmail.com>
2015-07-03 17:23:21 +0200

    Set up updating of place distance

--- ChasingPictures/app/src/main/java/de/fu_berlin/cdv/chasingpictures/Menu.java
+++ ChasingPictures/app/src/main/java/de/fu_berlin/cdv/chasingpictures/Menu.java
@@ -5,32 +5,50 @@ import android.content.Intent;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
 import android.location.Location;
+import android.location.LocationListener;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import de.fu_berlin.cdv.chasingpictures.api.Place;
 
 
 public class Menu extends Activity {
 
     private static final int REQUEST_PICTURE_SELECTION = 0;
+    private TextView mPlaceDistance;
     private ImageView mImageNext;
     private Location location;
+    private Place place;
+
+    private final LocationListener distanceCalculatorListener = new LocationHelper2.EasyLocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            updatePlaceDistance(location);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
+        // find and assign view fields
         mImageNext = (ImageView) findViewById(R.id.imageNext);
+        mPlaceDistance = (TextView) findViewById(R.id.menuPlaceDistance);
+
+        // Filter the *next* image to grayscale
         ColorMatrix matrix = new ColorMatrix();
         matrix.setSaturation(0);
         ColorMatrixColorFilter cf = new ColorMatrixColorFilter(matrix);
         mImageNext.setColorFilter(cf);
 
-        // TODO: Send an API request with this location
-        // TODO: Use the first place received to set the *next* picture
-        // TODO: Register a listener to update the distance to that place
+        location = new LocationHelper2(this).getLastKnownLocation();
 
+        // TODO: Send an API request with this location and set *place* to the first result.
+        // TODO: Use place to set the *next* picture
+        // TODO: Register a the listener to update the distance to that place
     }
 
     public void goToPictureSelection(View view) {
@@ -49,5 +67,10 @@ public class Menu extends Activity {
                 }
                 break;
         }
+    }
+
+    private void updatePlaceDistance(Location location) {
+        int round = Math.round(place.distanceTo(location));
+        mPlaceDistance.setText(String.format("%d m", round));
     }
 }
