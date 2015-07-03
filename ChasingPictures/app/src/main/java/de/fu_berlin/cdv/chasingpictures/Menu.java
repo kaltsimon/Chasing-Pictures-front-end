d6f7844d3d40cf1c3e57f49db7ace05e33b7eca0
Simon <kaltsimon@gmail.com>
2015-07-03 11:22:07 +0200

    Add empty activity for menu

---
+++ ChasingPictures/app/src/main/java/de/fu_berlin/cdv/chasingpictures/Menu.java
@@ -0,0 +1,14 @@
+package de.fu_berlin.cdv.chasingpictures;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class Menu extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
