e7a4813c2d3ae360b75c6c75b0015eb20c6f3b0f
Simon <kaltsimon@gmail.com>
2015-07-03 18:22:41 +0200

    Download picture for found place and show it

--- ChasingPictures/app/src/main/java/de/fu_berlin/cdv/chasingpictures/Menu.java
+++ ChasingPictures/app/src/main/java/de/fu_berlin/cdv/chasingpictures/Menu.java
@@ -2,36 +2,58 @@ package de.fu_berlin.cdv.chasingpictures;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
 import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.File;
 import java.util.List;
 
 import de.fu_berlin.cdv.chasingpictures.api.LocationTask;
+import de.fu_berlin.cdv.chasingpictures.api.Picture;
 import de.fu_berlin.cdv.chasingpictures.api.Place;
 
 
 public class Menu extends Activity {
 
+    public static final Location BERLIN = new Location(LocationManager.PASSIVE_PROVIDER);
     private static final int REQUEST_PICTURE_SELECTION = 0;
+    private static final int MIN_TIME = 5000;
+    private static final int MIN_DISTANCE = 5;
+
+    // FIXME: Find a better place, this is currently directly at the town hall!
+    static {
+        BERLIN.setLatitude(52.518405);
+        BERLIN.setLongitude(13.408499);
+    }
+
     private TextView mPlaceDistance;
     private ImageView mImageNext;
-    private Location location;
     private Place place;
-
     private final LocationListener distanceCalculatorListener = new LocationHelper2.EasyLocationListener() {
         @Override
         public void onLocationChanged(Location location) {
             updatePlaceDistance(location);
         }
     };
+    private final LocationListener placeFinderListener = new LocationHelper2.EasyLocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            mLocationHelper.stopLocationUpdates(this);
+            sendLocationRequest(location);
+        }
+    };
+    private LocationHelper2 mLocationHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,8 +70,20 @@ public class Menu extends Activity {
         ColorMatrixColorFilter cf = new ColorMatrixColorFilter(matrix);
         mImageNext.setColorFilter(cf);
 
-        location = new LocationHelper2(this).getLastKnownLocation();
+        mLocationHelper = new LocationHelper2(this);
+        Location location = mLocationHelper.getLastKnownLocation();
 
+        if (location == null) {
+            // Let's just go to Berlin
+            location = BERLIN;
+            // And wait for a location
+            mLocationHelper.startLocationUpdates(placeFinderListener, MIN_TIME, MIN_DISTANCE);
+        }
+
+        sendLocationRequest(location);
+    }
+
+    private void sendLocationRequest(@NonNull final Location location) {
         new LocationTask(this) {
             @Override
             protected void onPostExecute(@Nullable List<Place> places) {
@@ -60,10 +94,42 @@ public class Menu extends Activity {
                 // Check for null, just to be sure
                 if (place == null) return;
 
-                // TODO: Use place to set the *next* picture
-                // TODO: Register a the listener to update the distance to that place
+                updatePictureNext(place.getFirstPicture());
+                updatePlaceDistance(location);
+
+                // Register the listener to always show the current distance to that place
+                mLocationHelper.startLocationUpdates(distanceCalculatorListener, MIN_TIME, MIN_DISTANCE);
             }
-        };
+        }.execute(location);
+    }
+
+    private void updatePictureNext(final Picture picture) {
+        new PictureDownloader(getCacheDir()) {
+            private Bitmap pictureNextBitmap;
+
+            @Override
+            protected Void doInBackground(Picture... params) {
+                super.doInBackground(params);
+
+                File cachedFile = picture.getCachedFile();
+
+                if (cachedFile != null) {
+                    pictureNextBitmap = BitmapFactory.decodeFile(cachedFile.getPath());
+                }
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                updatePictureNext(pictureNextBitmap);
+            }
+        }.execute(picture);
+    }
+
+    private void updatePictureNext(Bitmap pictureNextBitmap) {
+        if (pictureNextBitmap != null)
+            mImageNext.setImageBitmap(pictureNextBitmap);
     }
 
     public void goToPictureSelection(View view) {
@@ -85,7 +151,9 @@ public class Menu extends Activity {
     }
 
     private void updatePlaceDistance(Location location) {
-        int round = Math.round(place.distanceTo(location));
-        mPlaceDistance.setText(String.format("%d m", round));
+        if (location != null && place != null) {
+            int round = Math.round(place.distanceTo(location));
+            mPlaceDistance.setText(String.format("%d m", round));
+        }
     }
 }
