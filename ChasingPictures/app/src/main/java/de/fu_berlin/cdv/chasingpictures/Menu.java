a2f6b58687bdafe34d3f6b62da77a0fe7448df03
Simon <kaltsimon@gmail.com>
2015-07-03 16:04:41 +0200

    Make picture for 'next' grayscale

--- ChasingPictures/app/src/main/java/de/fu_berlin/cdv/chasingpictures/Menu.java
+++ ChasingPictures/app/src/main/java/de/fu_berlin/cdv/chasingpictures/Menu.java
@@ -1,14 +1,25 @@
 package de.fu_berlin.cdv.chasingpictures;
 
 import android.app.Activity;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 
 public class Menu extends Activity {
+
+    private ImageView mImageNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        mImageNext = (ImageView) findViewById(R.id.imageNext);
+        ColorMatrix matrix = new ColorMatrix();
+        matrix.setSaturation(0);
+        ColorMatrixColorFilter cf = new ColorMatrixColorFilter(matrix);
+        mImageNext.setColorFilter(cf);
     }
 }
