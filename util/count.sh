1600f63dc9f4943c9fcea93a9049610396b91f0e
Simon Kalt <kaltsimon@users.noreply.github.com>
2015-07-10 11:57:49 +0200

    Also count kotlin code

--- util/count.sh
+++ util/count.sh
@@ -2,7 +2,9 @@
 # Usage: util/count.sh
 
 java_lines=$(for f in $(find . -name "*.java" | grep -v "build"); do cat "$f"; done | wc -l | tr -d '[:space:]')
+kotlin_lines=$(for f in $(find . -name "*.kt" | grep -v "build"); do cat "$f"; done | wc -l | tr -d '[:space:]')
 xml_lines=$(for f in $(find . -name "*.xml" | grep -v -e "build" -e ".idea"); do cat "$f"; done | wc -l | tr -d '[:space:]')
 
 echo "$java_lines lines of Java code"
+echo "$kotlin_lines lines of Kotlin code"
 echo "$xml_lines lines of XML definitions"
