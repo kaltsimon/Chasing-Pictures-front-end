41fbb128b225c34202f2b90a251a29102a053932
Simon Kalt <kaltsimon@users.noreply.github.com>
2015-07-16 14:07:29 +0200

    Move text-linkifying to Utilities class

--- ChasingPictures/app/src/main/java/de/fu_berlin/cdv/chasingpictures/util/Utilities.java
+++ ChasingPictures/app/src/main/java/de/fu_berlin/cdv/chasingpictures/util/Utilities.java
@@ -1,7 +1,16 @@
 package de.fu_berlin.cdv.chasingpictures.util;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.annotation.StringRes;
+import android.text.Html;
+import android.text.Spannable;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.URLSpan;
+import android.text.style.UnderlineSpan;
+import android.text.util.Linkify;
+import android.widget.TextView;
 
 import xdroid.toaster.Toaster;
 
@@ -47,5 +56,85 @@ public class Utilities {
         String message = context.getString(resID);
         message = args.length == 0 ? message : String.format(message, args);
         Toaster.toast(message);
+    }
+
+    /**
+     * Remove the underlines from the links in a text spannable.
+     *
+     * @param text A spannable containing the text
+     * @return The same spannable, edited in place
+     */
+    public static Spannable removeURLUnderlines(Spannable text) {
+        for (URLSpan u : text.getSpans(0, text.length(), URLSpan.class)) {
+            text.setSpan(new UnderlineSpan() {
+                @Override
+                public void updateDrawState(@NonNull TextPaint tp) {
+                    super.updateDrawState(tp);
+                    tp.setUnderlineText(false);
+                }
+            }, text.getSpanStart(u), text.getSpanEnd(u), 0);
+        }
+        return text;
+    } // Nothing
+
+
+    /**
+     * Retrieve the given string resource and parse it as HTML.
+     * <p/>
+     * The links will be underlined.
+     *
+     * @param context   The current context
+     * @param textResId The id of the string resource to retrieve
+     */
+    public static Spannable getTextAsHtml(Context context, @StringRes int textResId) {
+        return getTextAsHtml(context, textResId, true);
+    }
+
+    /**
+     * Retrieve the given string resource and parse it as HTML.
+     *
+     * @param context    The current context
+     * @param textResId  The id of the string resource to retrieve
+     * @param underlines Whether or not the links should be underlined
+     */
+    public static Spannable getTextAsHtml(Context context, @StringRes int textResId, boolean underlines) {
+        Spannable text = (Spannable) Html.fromHtml(context.getString(textResId));
+        if (!underlines)
+            text = removeURLUnderlines(text);
+        return text;
+    }
+
+    private static void setLinkifiedText(TextView textView, Spannable textWithLinks) {
+        textView.setText(textWithLinks);
+        Linkify.addLinks(textView, Linkify.ALL);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+    /**
+     * Set the text for the given {@link TextView} to the given resource and turn contained links
+     * (either just URLs or HTML &lt;a&gt elements) into clickable ones.
+     * <p/>
+     * Links will be underlined.
+     *
+     * @param context   The current context
+     * @param textView  The text view for which to set the text
+     * @param textResId The id of the string resource to load
+     */
+    public static void setLinkifiedText(Context context, TextView textView, @StringRes int textResId) {
+        setLinkifiedText(context, textView, textResId, true);
+    }
+
+    /**
+     * Set the text for the given {@link TextView} to the given resource and turn contained links
+     * (either just URLs or HTML &lt;a&gt elements) into clickable ones.
+     *
+     * @param context    The current context
+     * @param textView   The text view for which to set the text
+     * @param textResId  The id of the string resource to load
+     * @param underlines Whether or not links should be underlined
+     */
+    public static void setLinkifiedText(Context context, TextView textView, @StringRes int textResId, boolean underlines) {
+        Spannable text = getTextAsHtml(context, textResId, underlines);
+        setLinkifiedText(textView, text);
     }
 }
